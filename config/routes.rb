ee282b85245278cbc8ef36b425903f74d6af4ff8
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-18 19:33:41 -0300

    add Edit password view/controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # GET /about
   get 'about', to: 'about#index'
 
+  get 'password', to: 'passwords#edit'
+  patch 'password', to: 'passwords#update', as: :edit_password
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
 
